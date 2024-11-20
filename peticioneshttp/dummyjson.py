1262f4aa9ce1e859a22fa2f30bffcc9ef8f27185
eva <evagallardor@icloud.com>
2024-11-20 12:09:52 +0100

    LM

--- peticioneshttp/dummyjson.py
+++ peticioneshttp/dummyjson.py
@@ -2,11 +2,15 @@ import requests
 
 r = requests.get('https://dummyjson.com/users')
 users = r.json()
-print(users.keys())
-lista_users = users.get('users')
+#print(users.keys())
+lista_users = users.get("users")
+#print(lista_users)
 primer_usuario = lista_users[0]
+#print(primer_usuario.keys())
 nombre_usuario = primer_usuario.get("username")
 contraseña_usuario = primer_usuario.get("password")
+print(nombre_usuario)
+print(contraseña_usuario)
 
 data = {
     "username": nombre_usuario,
@@ -14,7 +18,6 @@ data = {
 }
 url = 'https://dummyjson.com/user/login'
 response = requests.post(url, data=data)
-
 print(response.json())
 
 
