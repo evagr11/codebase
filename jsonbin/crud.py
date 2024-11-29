fe00b971978179fe5598c9178d1193fff181b216
eva <evagallardor@icloud.com>
2024-11-29 11:57:40 +0100

    29/11/24

--- jsonbin/crud.py
+++ jsonbin/crud.py
@@ -1,33 +1,36 @@
-import os
 import requests
+import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
+bin_id = os.getenv("BIN_ID")
 XMasterKey = os.getenv("X_MASTER_KEY")
 XAccessKey = os.getenv("X_ACCESS_KEY")
-bin_id = os.getenv("BIN_ID")
 
-print(XMasterKey)
 url_root = "https://api.jsonbin.io/v3"
-route = f"/b/{bin_id}" 
+route = f"/b/{bin_id}"
 headers = {
+    "Content-Type": "application/json",
     "X-Master-Key": XMasterKey,
     "X-Access-Key": XAccessKey
 }
-
 usuario = {
-    "username": "aaaaa",
-    "password": "bbbbb"
-}
-
+    "user": "admin",
+    "password" : "1234"
+    }
 def read_db():
-    response = requests.get(url_root + route, headers=headers)
-    print(response.json())
+    r = requests.get(url_root + route, headers=headers)
+    print(r.json())
 
 def update_db():
-    response = requests.put(url_root + route, headers=headers, json=usuario)
-    print(response.json())
+    r = requests.put(url_root + route, headers=headers, json=usuario)
+    print(r.json())
+
+def add_user():
+    r = requests.patch(url_root + route, headers=headers, json=usuario)
+    print(r.json())
 
 read_db()
-update_db()
+add_user()
 read_db()
