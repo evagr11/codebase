f286f86e8d96e30e00ae897e9771984ef6d0858a
evagr11 <evagallardor@icloud.com>
2025-02-12 11:53:15 +0100

    Update tareas.py

--- intro_sqlite/app-tareas/tareas.py
+++ intro_sqlite/app-tareas/tareas.py
@@ -8,48 +8,63 @@ def create_table():
         c.executescript(f.read())
     conn.commit()
 
-#create_table()
 
-def add_task(title, status, desc=None):
-    c.execute("INSERT INTO tasks (title, descripcion,status) VALUES (?,?,?)", (title, desc, status))
-    conn.commit
-
-
-# TODO:
-# Funcion change_status
-# Funcion show_tasks
-# Funcion delete_tasks
-
-import sqlite3
-
-conn = sqlite3.connect("tareas.db")
-c = conn.cursor()
-
-def create_table():
-    with open("initdb.sql", "r") as f:
-        c.executescript(f.read())
+def add_task(title, desc=None):
+    c.execute("INSERT INTO tasks (title, description) VALUES (?,?)", (title, desc))
     conn.commit()
 
-def add_task(title, status, desc=None):
-    c.execute("INSERT INTO tasks (title, descripcion, status) VALUES (?, ?, ?)", (title, desc, status))
-    conn.commit()
-
-def change_status(task_id, new_status):
-    c.execute("UPDATE tasks SET status = ? WHERE id = ?", (new_status, task_id))
-    conn.commit()
-
-def show_tasks():
+def get_tasks():
     c.execute("SELECT * FROM tasks")
-    tasks = c.fetchall()
-    for task in tasks:
-        print(task)
+    return c.fetchall()
+
+def complete_task(task_id):
+    c.execute('UPDATE tasks SET status = "completed" WHERE id = ?', (task_id,))
+    conn.commit()
+
+def complete_task_by_title(title):
+    c.execute('UPDATE tasks SET status = "completed" WHERE title = ?', (title,))
+    conn.commit()
 
 def delete_task(task_id):
-    c.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
+    c.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
     conn.commit()
 
-# Ejemplo de uso:
-# add_task("Comprar pan", "pendiente")
-# show_tasks()
-# change_status(1, "completado")
-# delete_task(1)
+create_table()
+#complete_task(1)
+#add_task("tarea test", "desc test")
+#resultado = get_tasks()
+#print(resultado) 
+#= a poner print(get_taks())
+
+
+while True:
+    print('1. Add task')
+    print('2. List tasks')
+    print('3. Complete task')
+    print('4. Delete task')
+    print('5. Exit')
+
+    option = input('Choose an option:')
+
+    if option == '1':
+        task = input('Enter the task:')
+        desc = input('Enter the description: (optional)')
+        add_task(task, desc)
+
+    elif option == '2':
+        tasks = get_tasks()
+        print('ID | Task | Description | Status')
+        for task in tasks:
+            line = f'{task[0]} | {task[1]} | {task[2]} | {task[3]}'
+            print(line)
+
+    elif option == '3':
+        task_id = input('Enter the task id: ')
+        complete_task(task_id)
+
+    elif option == '4':
+        task_id = input('Enter the task id: ')
+
+    elif option == '5':
+        break
+
