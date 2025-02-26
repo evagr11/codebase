6cf56903ae41c178c38119e2f88e3d903db458ce
eva <evagallardor@icloud.com>
2025-02-26 12:01:36 +0100

    baseDeDatos

--- juego_p5/test_backend.sh
+++ juego_p5/test_backend.sh
@@ -1,4 +1,6 @@
-
+#este script testea los endpoint
+#GET /usuarios
+#POST /usuarios
 curl localhost:3000/usuarios
 
 curl -X POST http://localhost:3000/usuario -H "Content-Type: application/json" -d '{"nombre": "Juan", "edad": 30, "correo": "juan@example.com"}'
