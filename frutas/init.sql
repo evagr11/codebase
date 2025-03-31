a222560867932c0bd00ea81d71f90af8cdec268a
eva <evagallardor@icloud.com>
2025-03-31 12:44:37 +0200

    tareas

---
+++ frutas/init.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS frutas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    nombre TEXT NOT NULL                
+);
+
+-- Insertar datos iniciales en la tabla frutas
+INSERT INTO frutas (nombre) VALUES ('🍎');
+INSERT INTO frutas (nombre) VALUES ('🍐');
+INSERT INTO frutas (nombre) VALUES ('🍌');
+INSERT INTO frutas (nombre) VALUES ('🍓');
+INSERT INTO frutas (nombre) VALUES ('🍇');
+INSERT INTO frutas (nombre) VALUES ('🍉');
+INSERT INTO frutas (nombre) VALUES ('🍈');
+INSERT INTO frutas (nombre) VALUES ('🍒');
+INSERT INTO frutas (nombre) VALUES ('🍑');
+INSERT INTO frutas (nombre) VALUES ('🍍');
+INSERT INTO frutas (nombre) VALUES ('🥝');
+INSERT INTO frutas (nombre) VALUES ('🥑');
+INSERT INTO frutas (nombre) VALUES ('🥭');
+INSERT INTO frutas (nombre) VALUES ('🥥');
+INSERT INTO frutas (nombre) VALUES ('🍊');
--- frutas/server.js
+++ frutas/server.js
@@ -1,21 +1,32 @@
 const express = require("express")
+const sqlite3 = require("sqlite3")
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
-const frutas = ["manzana"]
+const frutas = ["🍎"]
 
 app.get("/frutas", (req, res) => {
-  res.json(frutas)
+  frutas.db("SELECT nombre FROM frutas", (req, rows) => {
+    const frutas = rows.map((row) => row.nombre);
+    res.json(frutas);
+  });
 })
 
 app.post("/frutas", (req, res) => {
-    const fruta = req.body.fruta;
-    console.log("Esto ha llegado desde el cliente:", fruta)
-    frutas.push(fruta)
-    res.json(frutas)
+  const fruta = req.body.fruta;
+  frutas.db("INSERT INTO frutas (nombre) VALUES (?)", [fruta], (req, rows) => {
+    const frutas = rows.map((row) => row.nombre);
+    res.json(frutas);
+  });
+  console.log("Esto ha llegado desde el cliente:", fruta)
+  frutas.push(fruta)
+  res.json(frutas)
 })
 
 app.listen(3000, () => console.log("Servidor funcionando en 3000"))
+
+//Sustituir en backend lo del array por una bbdd de sqlite
+
